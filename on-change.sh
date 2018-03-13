90e34d05274bd2a76e5e0b6613a18c4ac0fd3840
ziezo <36749330+ziezo@users.noreply.github.com>
2018-03-13 15:50:00 +0100

    on-change.sh

--- on-change.sh
+++ on-change.sh
@@ -4,18 +4,18 @@ function copy_files {
   IFS=':' read -r -a array <<< "$2"
   for fn in "${array[@]}"
   do
-    echo cp "$1" "$fn"
+    echo "cp $1 $fn"
+    cp "$1" "$fn"
   done
 }
-
 
 copy_files /cert/fullchain.pem "${COPY_FULLCHAIN}"
 copy_files /cert/privkey.pem "${COPY_PRIVKEY}"
 copy_files /cert/chain.pem "${COPY_CHAIN}"
 copy_files /cert/cert.pem "${COPY_CERT}"
 
-if [ -n "$CONTAINERS_TO_RESTART" ] ; then
-  docker restart $CONTAINERS_TO_RESTART
+if [ -n "$RESTART" ] ; then
+  echo docker restart $RESTART
+  docker restart $RESTART
 fi
-
 
